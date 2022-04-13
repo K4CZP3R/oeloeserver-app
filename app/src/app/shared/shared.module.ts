c31a4bbf2b6e2b2ed3d2e46679959f6690965dcc
Kacper Serewiś <kacper@kacperserewis.net>
2022-04-13 18:06:17 +0200

    export shared modules

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RelativeTimePipe } from './pipes/relative-time.pipe';
+import { ReadableTimePipe } from './pipes/readable-time.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [RelativeTimePipe, ReadableTimePipe],
   imports: [CommonModule, HttpClientModule],
-  exports: [CommonModule],
+  exports: [CommonModule, RelativeTimePipe, ReadableTimePipe],
 })
 export class SharedModule {}
