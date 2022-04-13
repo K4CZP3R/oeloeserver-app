fe5adf11fcb9e100d2b36d916a62d9fc7df7300f
Kacper Serewiś <kacper@kacperserewis.net>
2022-04-13 18:04:45 +0200

    implement logic for players card

--- app/src/app/landing/components/players-card/players-card.component.ts
+++ app/src/app/landing/components/players-card/players-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeIByI } from 'src/app/core/animations/fade-ibyi.animation';
+import { OeloeserverService } from 'src/app/shared/services/oeloeserver.service';
+import { PlayerState } from 'src/app/shared/models/player-state.interface';
+import { OeloeserverWsService } from 'src/app/shared/services/oeloeserver-ws.service';
 
 @Component({
   selector: 'app-players-card',
@@ -8,7 +11,23 @@ import { fadeIByI } from 'src/app/core/animations/fade-ibyi.animation';
   animations: [],
 })
 export class PlayersCardComponent implements OnInit {
-  constructor() {}
+  constructor(private oeloeserver: OeloeserverService) {
+    this.oeloeserver.getWsMessages().subscribe((s) => this.fetchData());
+  }
 
-  ngOnInit(): void {}
+  playersStats: PlayerState[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.oeloeserver.getAllPlayerStatus().then((states) => {
+      this.playersStats = states.data;
+    });
+  }
+
+  getTime(unix: number) {
+    return new Date(unix);
+  }
 }
