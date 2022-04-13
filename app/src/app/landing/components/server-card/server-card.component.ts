277c0365e066399c7007bb71827ee25da2ee177f
Kacper Serewiś <kacper@kacperserewis.net>
2022-04-13 18:04:30 +0200

    use assets

--- app/src/app/landing/components/server-card/server-card.component.ts
+++ app/src/app/landing/components/server-card/server-card.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { ServerStatus } from 'src/app/shared/models/server-status.interface';
+import { OeloeserverService } from 'src/app/shared/services/oeloeserver.service';
 
 @Component({
   selector: 'app-server-card',
   templateUrl: './server-card.component.html',
-  styleUrls: ['./server-card.component.scss']
+  styleUrls: ['./server-card.component.scss'],
 })
 export class ServerCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public serverStatus?: ServerStatus;
+  constructor(private oeloeserver: OeloeserverService) {
+    this.oeloeserver.getWsMessages().subscribe((s) => this.queryData());
   }
 
+  ngOnInit(): void {
+    this.queryData();
+  }
+
+  queryData() {
+    this.oeloeserver
+      .getServerStatus()
+      .then((ss) => (this.serverStatus = ss.data));
+  }
 }
