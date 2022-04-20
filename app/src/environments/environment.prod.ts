7a445b9ad6314d5bf35ef14792a0ea897c2b9fcc
Kacper Serewiś <kacper@kacperserewis.net>
2022-04-20 18:22:08 +0200

    fix prod env

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  oeloeserver_ws: 'ws://localhost:8080',
+  oeloeserver_rest: 'http://localhost:1337',
+  application_name: '',
+  mc_server_addr: 'mc.hypixel.net:25565',
 };
