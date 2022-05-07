084d12aa8dc60c02736dc3ea8f9f7bf705c42477
Kacper Serewiś <kacper@kacperserewis.net>
2022-05-07 18:53:21 +0200

    Update environment.prod.ts

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  oeloeserver_ws: 'wss://wsapi.mc.k4czp3r.xyz',
-  oeloeserver_rest: 'https://api.mc.k4czp3r.xyz',
+  oeloeserver_ws: 'wss://wsapimc.k4czp3r.xyz',
+  oeloeserver_rest: 'https://apimc.k4czp3r.xyz',
   application_name: 'Oeloes',
   mc_server_addr: 'mc.k4czp3r.xyz:25266',
 };
